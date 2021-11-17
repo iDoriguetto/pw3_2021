d57d2228342c4b2d3b562f20cbd76efec72787f1
Isabella Doriguetto <dorighetta007@gmail.com>
2021-11-17 16:02:31 -0300

    Aula dia 04/11

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -69,6 +69,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('countries.index')}}">Paises</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('movies.index')}}">Filmes</a>
+                    </li>
                 </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
